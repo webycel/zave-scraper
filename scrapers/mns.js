23d76e00988d726d16db854964d5a54111a99f8c
webycel <anthony.web91@gmail.com>
2016-01-27 15:31:34 +0000

    add more info to mns product

--- scrapers/mns.js
+++ scrapers/mns.js
@@ -30,18 +30,27 @@ var MnsScraper = {
         productDetails = {};
 
     productDetails["name"] = $('.product-title h1').text().trim();
+    productDetails["productImage"] = $('.mainProd img.zoom').attr('src') || $('.mainProd img.zoom').attr('srcset').split(" 1024w")[0];
     productDetails["productCode"] = $('input[name="productCode"]').attr('value');
     productDetails["productID"] = $('.information .code').text().trim();
     productDetails["price"] = $('.pricing .price1 span').text().trim();
     productDetails["colours"] = [];
 
+    console.log(productDetails);
+
     // get colours
-    $('.swatch-container .swatches li input').each(function(i, swatch) {
+    $('.swatch-container .swatches li label').each(function(i, swatch) {
       var prod = {
-        id: $(swatch).attr('value'),
-        name: $(swatch).next().data('swatch-name'),
-        image: $(swatch).data('image-set')
+        id: $(swatch).prev().attr('value'),
+        name: $(swatch).data('swatch-name'),
+        image: $(swatch).css('background-image')
       };
+
+      prod.sizeKey = productDetails["productCode"] + '_' + prod.id;
+
+      if (prod.image.indexOf("url") >= 0) {
+        prod.image = prod.image.replace("url(", "").replace(")", "");
+      }
 
       productDetails["colours"].push(prod);
     });
